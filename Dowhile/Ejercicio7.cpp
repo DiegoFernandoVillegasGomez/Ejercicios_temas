4192198e6fb187ffe355734ea05c305e94ee6371
Diego Villegas <00109825@uca.edu.sv>
2025-06-06 12:20:02 -0600

    Ejercicio 7 terminado

---
+++ Dowhile/Ejercicio7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "Ingrese un numero" << endl;
+    cin >> n;
+
+    while (n >= 0)
+    {
+
+        cout << n << endl;
+        n--;
+    }
+
+    return 0;
+}
